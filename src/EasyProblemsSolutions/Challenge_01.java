0c11e558eaf3e687e1f2c02d578cd0bcb837d8a0
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-10 11:39:10 +0100

    Documentation of Challenge_01

--- src/EasyProblemsSolutions/Challenge_01.java
+++ src/EasyProblemsSolutions/Challenge_01.java
@@ -2,6 +2,18 @@ package EasyProblemsSolutions;
 
 public class Challenge_01 {
 
+    /**
+     * Running Sum of 1d Array
+     * Given an array of numbers, this function returns a new array where each element
+     * at index `i` is the sum of all the elements in the input array from index `0` to `i`.
+     * Example:
+     * Input: nums = [1,2,3,4]
+     * Output: [1, 3, 6, 10]
+     * Explanation:
+     * The running sum is computed as follows:
+     * [1, 1+2, 1+2+3, 1+2+3+4].
+     */
+
     public static void main(String[] args) {
 
     }
